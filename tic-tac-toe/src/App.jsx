61e31e116712b762ff83ba5c3917d4ee170283d1
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2024-10-06 19:19:12 +0530

    Tic-Tac-Toe app deployed

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './componenets/Card/Card'
+import Grid from './componenets/Grid/Grid'
+
+function App() {
+  
+  return (
+    <>
+      <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
